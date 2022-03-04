f2f517b15bbde7c99c1c9d80b57f3a7d0f0a660d
dhwani patel <dhwanipatel.itb.socet@gmail.com>
2022-03-04 18:45:13 +0530

    7th Commit

---
+++ CRUDinMVC/CRUDinMVC/Models/IStudentDBHandle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CRUDinMVC.Models
+{
+    public interface IStudentDBHandle
+    {
+        bool AddStudent(StudentModel smodel);
+        bool DeleteStudent(int id);
+        List<StudentModel> GetStudent();
+        bool UpdateDetails(StudentModel smodel);
+    }
+}
