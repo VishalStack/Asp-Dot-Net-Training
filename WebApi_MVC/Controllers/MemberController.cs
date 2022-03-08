7b4573703b9f6d8854db05f79c77e046f405f0d5
naxi2312 <naxi.s@asambhav.in>
2022-03-08 18:18:13 +0530

    web api first file is commited

---
+++ WebApi_MVC/Controllers/MemberController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi_MVC.Models;
+
+namespace WebApi_MVC.Controllers
+{
+    public class MemberController : ApiController
+    {
+        MemberDataClassDataContext db = new MemberDataClassDataContext();
+
+
+
+        //This action method return all members records.  
+        // GET api/<controller>  
+        public IEnumerable<tblMember> Get()
+        {
+            //returning all records of table tblMember.  
+            return db.tblMembers.ToList().AsEnumerable();
+        }
+
+
+
+        //This action method will fetch and filter for specific member id record  
+        // GET api/<controller>/5  
+        public HttpResponseMessage Get(int id)
+        {
+            //fetching and filter specific member id record   
+            var memberdetail = (from a in db.tblMembers where a.MemberID == id select a).FirstOrDefault();
+
+
+            //checking fetched or not with the help of NULL or NOT.  
+            if (memberdetail != null)
+            {
+                //sending response as status code OK with memberdetail entity.  
+                return Request.CreateResponse(HttpStatusCode.OK, memberdetail);
+            }
+            else
+            {
+                //sending response as error status code NOT FOUND with meaningful message.  
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+            }
+        }
+
+
+        //To add a new member record  
+        // POST api/<controller>  
+        public HttpResponseMessage Post([FromBody] tblMember _member)
+        {
+            try
+            {
+                //To add an new member record  
+                db.tblMembers.InsertOnSubmit(_member);
+
+                //Save the submitted record  
+                db.SubmitChanges();
+
+                //return response status as successfully created with member entity  
+                var msg = Request.CreateResponse(HttpStatusCode.Created, _member);
+
+                //Response message with requesturi for check purpose  
+                msg.Headers.Location = new Uri(Request.RequestUri + _member.MemberID.ToString());
+
+                return msg;
+            }
+            catch (Exception ex)
+            {
+
+                //return response as bad request  with exception message.  
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+
+        //To update member record  
+        // PUT api/<controller>/5  
+        public HttpResponseMessage Put(int id, [FromBody] tblMember _member)
+        {
+            //fetching and filter specific member id record   
+            var memberdetail = (from a in db.tblMembers where a.MemberID == id select a).FirstOrDefault();
+
+            //checking fetched or not with the help of NULL or NOT.  
+            if (memberdetail != null)
+            {
+                //set received _member object properties with memberdetail  
+                memberdetail.MemberName = _member.MemberName;
+                memberdetail.PhoneNumber = _member.PhoneNumber;
+                //save set allocation.  
+                db.SubmitChanges();
+
+                //return response status as successfully updated with member entity  
+                return Request.CreateResponse(HttpStatusCode.OK, memberdetail);
+            }
+            else
+            {
+                //return response error as NOT FOUND  with message.  
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+            }
+
+
+        }
+
+        // DELETE api/<controller>/5  
+        public HttpResponseMessage Delete(int id)
+        {
+
+            try
+            {
+                //fetching and filter specific member id record   
+                var _DeleteMember = (from a in db.tblMembers where a.MemberID == id select a).FirstOrDefault();
+
+                //checking fetched or not with the help of NULL or NOT.  
+                if (_DeleteMember != null)
+                {
+
+                    db.tblMembers.DeleteOnSubmit(_DeleteMember);
+                    db.SubmitChanges();
+
+                    //return response status as successfully deleted with member id  
+                    return Request.CreateResponse(HttpStatusCode.OK, id);
+                }
+                else
+                {
+                    //return response error as Not Found  with exception message.  
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Member Not Found or Invalid " + id.ToString());
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                //return response error as bad request  with exception message.  
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+    }
+}
