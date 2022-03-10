ba94792330e68ed0359a8d2c20454922e11b364f
dhwani patel <dhwanipatel.itb.socet@gmail.com>
2022-03-10 17:45:53 +0530

    10th commit

---
+++ MVCAuthentication(9march)/MVCAuthentication(9march)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAuthentication_9march_.MvcApplication" Language="C#" %>
---
+++ MVCAuthentication(9march)/MVCAuthentication(9march)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAuthentication_9march_.Startup))]
+namespace MVCAuthentication_9march_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
