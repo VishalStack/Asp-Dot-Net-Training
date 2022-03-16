7fe3ecfc02f404fd73e547fa0021f6daf5aec2e8
ekta parmar <ektaparmar1706@gmail.com>
2022-03-16 18:20:24 +0530

    fourteenth commit

---
+++ WebApiMvc/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiMvc.Models;
+
+namespace WebApiMvc.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public IHttpActionResult GetProduct()
+        {
+            TestEntities cm = new TestEntities();
+            var results = cm.Product.ToList();
+            return Ok(results);
+        }
+
+    }
+}
