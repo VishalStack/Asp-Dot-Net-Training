d6bd83130d4b54945d9e115827992bdd6c3a98d3
dhwani patel <dhwanipatel.itb.socet@gmail.com>
2022-03-24 21:15:53 +0530

    16th commit

---
+++ WebApiPagging/WebApiPagging/ProductResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPagging
+{
+    public class ProductResponse
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
