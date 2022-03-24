3f014a888e5d24f7adefea5811e4b1ac402778b2
Dhwani patel <84832258+dhwanipatel2301@users.noreply.github.com>
2022-03-24 21:13:51 +0530

    Delete Pagging directory

--- Pagging/Pagging/ProductResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pagging
-{
-    public class ProductResponse
-    {
-        public List<Product> Products { get; set; } = new List<Product>();
-        public int Pages { get; set; }
-        public int CurrentPage { get; set; }
-    }
-}
