cdeb40ca067691a9fbca5ba2587fc2e1f7047e66
ekta parmar <ektaparmar1706@gmail.com>
2022-03-30 16:44:42 +0530

    twentieth commit

---
+++ DotvvmApplication1/DAL/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DotvvmApplication1.DAL.Entities;
+
+namespace DotvvmApplication1.DAL
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ DotvvmApplication1/ViewModels/CRUD/EditViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Runtime.Filters;
+using DotvvmApplication1.Models;
+using DotvvmApplication1.Services;
+
+namespace DotvvmApplication1.ViewModels.CRUD
+{
+    public class EditViewModel : MasterPageViewModel
+    {
+        private readonly StudentService studentService;
+
+        public StudentDetailModel Student { get; set; }
+
+        [FromRoute("Id")]
+        public int Id { get; private set; }
+
+        public EditViewModel(StudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+
+        public override async Task PreRender()
+        {
+            if (Id != 0)
+            {
+                Student = await studentService.GetStudentByIdAsync(Id);
+            }
+            await base.PreRender();
+        }
+
+        public async Task EditStudent()
+        {
+            await studentService.UpdateStudentAsync(Student);
+            Context.RedirectToRoute("CRUD_Detail", new { Id = Id });
+        }
+    }
+}
