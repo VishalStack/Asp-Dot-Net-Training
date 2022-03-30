d446c4019a645dd19c738ac9e39d5d264948bfa0
ekta parmar <ektaparmar1706@gmail.com>
2022-03-30 16:17:58 +0530

    twentieth commit

---
+++ DotvvmApplication/Controller/UserController.cs
@@ -0,0 +1,120 @@
+﻿using DotvvmApplication.DTO;
+using DotvvmApplication.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DotvvmApplication.Controller
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class UserController : ControllerBase
+        {
+             private readonly DotVVMContext DotVVMContext;  
+  
+        public UserController(DotVVMContext DotVVMContext)  
+        {  
+            this.DotVVMContext = DotVVMContext;  
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult<List<UserDTO>>> Get()
+        {
+            var List = await DotVVMContext.Users.Select(
+                s => new UserDTO
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Username = s.Username,
+                    Password = s.Password,
+                    EnrollmentDate = s.EnrollmentDate
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        [HttpGet("GetUserById")]
+        public async Task<ActionResult<UserDTO>> GetUserById(int Id)
+        {
+            UserDTO User = await DotVVMContext.Users.Select(
+                    s => new UserDTO
+                    {
+                        Id = s.Id,
+                        FirstName = s.FirstName,
+                        LastName = s.LastName,
+                        Username = s.Username,
+                        Password = s.Password,
+                        EnrollmentDate = s.EnrollmentDate
+                    })
+                .FirstOrDefaultAsync(s => s.Id == Id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return User;
+            }
+        }
+
+        [HttpPost("InsertUser")]
+        public async Task<HttpStatusCode> InsertUser(UserDTO User)
+        {
+            var entity = new User()
+            {
+                FirstName = User.FirstName,
+                LastName = User.LastName,
+                Username = User.Username,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
+
+            DotVVMContext.Users.Add(entity);
+            await DotVVMContext.SaveChangesAsync();
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdateUser")]
+        public async Task<HttpStatusCode> UpdateUser(UserDTO User)
+        {
+            var entity = await DotVVMContext.Users.FirstOrDefaultAsync(s => s.Id == User.Id);
+
+            entity.FirstName = User.FirstName;
+            entity.LastName = User.LastName;
+            entity.Username = User.Username;
+            entity.Password = User.Password;
+            entity.EnrollmentDate = User.EnrollmentDate;
+
+            await DotVVMContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeleteUser/{Id}")]
+        public async Task<HttpStatusCode> DeleteUser(int Id)
+        {
+            var entity = new User()
+            {
+                Id = Id
+            };
+            DotVVMContext.Users.Attach(entity);
+            DotVVMContext.Users.Remove(entity);
+            await DotVVMContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
