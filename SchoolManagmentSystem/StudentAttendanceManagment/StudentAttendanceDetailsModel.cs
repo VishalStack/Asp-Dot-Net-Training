23bedce85c89c2c97cf41f06f492d9518aff0807
dhwani patel <dhwanipatel.itb.socet@gmail.com>
2022-04-01 12:15:17 +0530

    20th commit

---
+++ SchoolManagmentSystem/StudentAttendanceManagment/StudentAttendanceDetailsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentAttendanceManagment
+{
+    public class StudentAttendanceDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public double AttendencePercentage { get; set; }
+    }
+}
